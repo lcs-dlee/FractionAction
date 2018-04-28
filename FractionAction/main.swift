96d033f44758e2f40e357e4393845ebb5560e9a8
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2018-04-28 10:20:11 -0400

    Filtering Numerator/Denominator Input

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -8,6 +8,53 @@
 
 import Foundation
 
+//Filter User Input
+var validNumerator = 0
+var validDenominator = 0
+
+while true {
+    print("Numerator?")
+    guard let rawNumeratorInput = readLine() else {
+        continue
+    }
+    //Check numerator input is integer
+    guard let numeratorInput = Int(rawNumeratorInput) else {
+        continue
+    }
+    
+    //Check numerator input is non-negative integer
+    if numeratorInput < 0 {
+        print("Numerator cannot be negative.")
+        continue
+    }
+    validNumerator = numeratorInput
+    break
+    
+}
+
+while 1 == 1 {
+    
+    print("Denominator?")
+    guard let rawDenominatorInput = readLine() else {
+        continue
+    }
+    //Check denominator input is integer
+    guard let denominatorInput = Int(rawDenominatorInput) else {
+        continue
+    }
+    
+    //Check denominator input is positive integer
+    if denominatorInput <= 0 {
+        print("Denominator canot be non-positive integer")
+        continue
+    }
+    validDenominator = denominatorInput
+    break
+}
+print(validNumerator)
+print(validDenominator)
+
+
 // INPUT
 // Collect and filter user input here
 
